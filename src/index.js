2c90adcf85debe19300aff298930e01acb2d3ff1
Yasui Kohei <yasui.kohei@jp.fujitsu.com>
2022-12-20 17:48:30 +0900

    3. Storeを作成する。

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+import { createStore } from 'redux';
+import { Provider } from 'react';
+import reducer from "./reducers";
+import App from './components/App';
+
+/// storeの生成がされている
+const store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store= {store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
